6857c315e58d91bc4adca8d1d1c65a68c1fd15fe
Vidhi-B-Shah <vidhivk18@gmail.com>
2023-06-28 16:33:39 +0530

    app.py

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np 
 import pandas as pd 
+import pickle
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent.parent))
 from src.exception import CustomException
@@ -45,4 +46,11 @@ def evaluate_model(X_train, y_train,X_test, y_test, models, param):
            
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
